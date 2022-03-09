383e0e7ac0269ad6802f68048716cbeb879e4601
kemokaba <kemokaba@gmail.com>
2022-03-09 10:07:32 +0100

    link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoriqueComponent } from './historique/historique.component';
+import { ProduitComponent } from './produit/produit.component';
+import { StockComponent } from './stock/stock.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produit', component: ProduitComponent},
+  {path:'stock', component: StockComponent},
+  {path:'historique', component: HistoriqueComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
