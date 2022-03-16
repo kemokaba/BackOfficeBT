9d52eededdc2ed8dc0e53c1323a297f5b6e0f567
Mathieu Albin <mathieualbin@MacBook-Air-de-Mathieu.local>
2022-03-16 11:08:54 +0100

    save

--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -18,7 +18,7 @@ export class StockComponent implements OnInit {
   getProducts(){
     this.productsService.getProductsFromJson().subscribe((res : Product[]) => {
       this.listeProduits = res;
-      this.listeProduits.sort((a, b) => (a.id < b.id ? -1 : 1));
+      this.listeProduits.sort((a, b) => (a.tig_id < b.tig_id ? -1 : 1));
     },
     (err) => {
       alert('failed loading json data');
