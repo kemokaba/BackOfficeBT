413b471ac163977aa90785b5ca1169f83dfe14f9
kemokaba <kemokaba@gmail.com>
2022-03-24 14:43:24 +0100

    graph

--- src/app/historique/historique.component.ts
+++ src/app/historique/historique.component.ts
@@ -4,6 +4,8 @@ import { Product } from '../core/interfaces/product';
 import { ProductsService } from '../core/services/product.service';
 import { Transaction } from '../core/interfaces/transaction';
 import { Chart, registerables } from 'chart.js';
+import { RaisonModif } from '../core/interfaces/raisonModif';
+import { Data } from '@angular/router';
 @Component({
   selector: 'app-historique',
   templateUrl: './historique.component.html',
@@ -12,54 +14,50 @@ import { Chart, registerables } from 'chart.js';
 export class HistoriqueComponent implements OnInit {
 
   donneeHisto: Transaction[] = [];
-
- 
-  typeT: string[] = [];
-  dateT: any;
-  valeurT: number[] = [];
-  quantityT: any;
-  chart: any = [];
-
+  valeurA:number = 0
+  valeurV:number = 0
+  valeurI:number = 0
  
   
-   
-  constructor(private productsService: ProductsService){Chart.register(...registerables);}
+  saleData:Data [] = [];
+  ARV: any = [];
+
+
+  constructor(private productsService: ProductsService){}
   getDonneesHisto(){
     this.productsService.donneesHisto().subscribe((res : Transaction[]) => {
       this.donneeHisto = res;
       //console.log(this.donneeHisto)
        for (let donn of this.donneeHisto ) {
-
-          this.chart = new Chart('canvas', {
-            type: 'line',
-            data: {
-              labels: [donn.typeT],
-              datasets: [
-                {
-                  data: [donn.valeurT], 
-                  borderColor: '#3e95cd',
-                  fill: false,
-                  label: 'valeur',
-                  backgroundColor: 'rgba(93, 175, 89, 0.1)',
-                  borderWidth: 3,
-                },
-              ],
-            },
-          });
-
-        
-
-
-       
+          //console.log(donn.typeT)
+          if (donn.typeT == 'ajout'){
+            this.valeurA = this.valeurA + donn.valeurT
+             
+            continue
+          }
+          if (donn.typeT == 'retrait-par-vente'){
+            this.valeurV = this.valeurV + donn.valeurT
+             
+            continue
+          }
+          if (donn.typeT == 'retrait-par-invendus'){
+            this.valeurI = this.valeurI + donn.valeurT
+             
+            continue
+          }
        }
-      
+      this.saleData = [
+        { name: 'ajout', value: this.valeurA },
+        { name: 'vente', value: this.valeurV },
+        { name: 'invendu', value: this.valeurI}
+      ]
        
     },
     (err) => {
       alert('failed loading json data');
     });
 
-   
+   console.log(this.saleData)
   };
   
   ngOnInit(): void {
