021c83ab8d169244d83b2feba1f0b764089db84a
kemokaba <kemokaba@gmail.com>
2022-03-26 17:39:20 +0100

    visual

--- src/app/historique/historique.component.ts
+++ src/app/historique/historique.component.ts
@@ -2,72 +2,38 @@ import { Component, OnInit } from '@angular/core';
  
 import { Product } from '../core/interfaces/product';
 import { ProductsService } from '../core/services/product.service';
-import { Transaction} from '../core/interfaces/transaction';
-import {Categorie} from '../core/interfaces/categorie'
- 
+import { Transaction } from '../core/interfaces/transaction';
+import { RaisonModif } from '../core/interfaces/raisonModif';
 import { Data } from '@angular/router';
-import { id } from '@swimlane/ngx-charts';
 @Component({
   selector: 'app-historique',
   templateUrl: './historique.component.html',
   styleUrls: ['./historique.component.css']
 })
 export class HistoriqueComponent implements OnInit {
-  listeProduits: Product[] = [];
+
   donneeHisto: Transaction[] = [];
-  valeurA:number = 0
-  valeurV:number = 0
-  valeurI:number = 0
-  saleData:Data [] = [];
+
  
 
-  selectCat: Categorie[] = [
+  saleData:Data [] = [];
+  selected: number = -1
 
-    {nameCat:'Poissons', valCat: 0},
-    {nameCat: 'Coquillages', valCat: 1},
-    {nameCat: 'Crustaces', valCat: 2},
-
-  ];
-  selectedValue: string= "";
-
-  
-  triTableau(numCat: number){
-    let tabTri = this.donneeHisto.filter(produit => produit.category == numCat);
-    return tabTri;
-     
-    
-  }
 
   constructor(private productsService: ProductsService){}
+
+  triTableau(numCat: number){
+    let tabTri = this.donneeHisto.filter(transa => transa.category == numCat);
+    this.createSaleData(tabTri)
+    console.log(this.saleData)
+  }
+
   getDonneesHisto(){
     this.productsService.donneesHisto().subscribe((res : Transaction[]) => {
       this.donneeHisto = res;
-       
       //console.log(this.donneeHisto)
-       for (let donn of this.donneeHisto ) {
-          //console.log(donn.typeT)
-          if (donn.typeT == 'ajout'){
-            this.valeurA = this.valeurA + donn.valeurT
-             
-            continue
-          }
-          if (donn.typeT == 'retrait-par-vente'){
-            this.valeurV = this.valeurV + donn.valeurT
-             
-            continue
-          }
-          if (donn.typeT == 'retrait-par-invendus'){
-            this.valeurI = this.valeurI + donn.valeurT
-             
-            continue
-          }
-       }
-      this.saleData = [
-        { name: 'ajout', value: this.valeurA },
-        { name: 'vente', value: this.valeurV },
-        { name: 'invendu', value: this.valeurI}
-      ]
-       
+      this.createSaleData(this.donneeHisto)
+
     },
     (err) => {
       alert('failed loading json data');
@@ -75,16 +41,41 @@ export class HistoriqueComponent implements OnInit {
 
    console.log(this.saleData)
   };
+
+
+
+  createSaleData(tab:Transaction[]){
+      this.saleData.length = 0
+      let valeurA:number = 0
+      let valeurV:number = 0
+      let valeurI:number = 0
+      for (let donn of tab ) {
+        //console.log(donn.typeT)
+        if (donn.typeT == 'ajout'){
+          valeurA = valeurA + donn.valeurT
+
+          continue
+        }
+        if (donn.typeT == 'retrait-par-vente'){
+          valeurV = valeurV + donn.valeurT
+
+          continue
+        }
+        if (donn.typeT == 'retrait-par-invendus'){
+          valeurI = valeurI + donn.valeurT
+
+          continue
+        }
+    }
+    this.saleData = [
+      { name: 'ajout', value: valeurA },
+      { name: 'vente', value: valeurV },
+      { name: 'invendu', value: valeurI }
+    ]
+  }
   
   ngOnInit(): void {
     this.getDonneesHisto()
-     
-     
-
-    
-
-
-    
     
   }
    
