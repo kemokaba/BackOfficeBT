59f26f3c49482a76698ccc0303789e2b9cb70906
Mathieu Albin <mathieualbin@MacBook-Air-de-Mathieu.local>
2022-03-29 10:06:24 +0200

    Revert "ok"
    This reverts commit c169156f3a5b175fe1df6647695c6b37e61a140c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,10 @@ export class AppComponent {
     });
   }
 
+  cards = [
+    {title: 'Title 1', content: 'Content 1'},
+    {title: 'Title 2', content: 'Content 2'},
+    {title: 'Title 3', content: 'Content 3'},
+    {title: 'Title 4', content: 'Content 4'}
+  ];
 }
